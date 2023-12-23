d2dbc7dccdf77b42b9d7fd7d9ba895e0897e002c
Peter Molettiere <peter.molettiere@gmail.com>
2023-12-23 07:00:46 -0500

    New icon

---
+++ DragAlert/View/StrokeText.swift
@@ -0,0 +1,18 @@
+//
+//  StrokeText.swift
+//  Drag Alert
+//
+//  Created by Peter Molettiere on 12/22/23.
+//
+
+import SwiftUI
+
+struct StrokeText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StrokeText()
+}
---
+++ DragAlert/View/VolumeSlider.swift
@@ -0,0 +1,8 @@
+//
+//  VolumeSlider.swift
+//  Drag Alert
+//
+//  Created by Peter Molettiere on 12/22/23.
+//
+
+import Foundation
